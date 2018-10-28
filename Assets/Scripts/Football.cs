59078177a99fdff4c48a54624cc92d1cea028646
victorcesaroni <victor.cesaroni@gmail.com>
2018-10-28 01:00:02 -0300

    first commit

---
+++ Assets/Scripts/Football.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Football : MonoBehaviour
+{
+    public GameObject blueGoal;
+    public GameObject redGoal;
+    public GameObject ball;
+    public GameObject ballSpawn;
+    public Text textScoreBlue;
+    public Text textScoreRed;
+
+    public int blueScore;
+    public int redScore;
+
+    // Use this for initialization
+    void Start ()
+    {
+        blueScore = 0;
+        redScore = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnScore(GameObject goal)
+    {
+        if (goal == redGoal.gameObject)
+        {
+            ScoreBlue();
+        }
+        else if (goal == blueGoal.gameObject)
+        {
+            ScoreRed();
+        }
+    }
+
+    public void ScoreBlue()
+    {
+        blueScore++;
+        textScoreBlue.text = "A " + blueScore;
+        ball.transform.localPosition = ballSpawn.transform.localPosition;
+        ball.transform.position = ballSpawn.transform.position;
+    }
+
+    public void ScoreRed()
+    {
+        redScore++;
+        textScoreRed.text = redScore + " V";
+        ball.transform.localPosition = ballSpawn.transform.localPosition;
+        ball.transform.position = ballSpawn.transform.position;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject ball;
+    public GameObject defend;
+    public GameObject goal;
+
+    Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        bool shouldGo = Random.Range(0, 20) == 0;
+        float distanceToBall = (ball.transform.position - transform.position).magnitude;
+        float distanceToGoal = (goal.transform.position - transform.position).magnitude;
+        float distanceToDefend = (goal.transform.position - transform.position).magnitude;
+
+        if (shouldGo)
+        {
+            float force = Random.Range(10000.0f, 50000.0f);
+
+            transform.LookAt(ball.transform);
+            rigidbody.AddRelativeForce(0, 0, 10000.0f);
+
+            Vector2 forward = (ball.transform.position - transform.position).normalized;
+            rigidbody.AddForce(forward * force * Time.smoothDeltaTime);
+        }
+	}
+}
