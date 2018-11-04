0a22f87ac233ebe9c535e344135855b31467267b
victorcesaroni <victor.cesaroni@gmail.com>
2018-11-04 18:08:39 -0200

    tipo de ai dropdown

---
+++ Assets/Scripts/DropdownAiType.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownAiType : MonoBehaviour {
+
+    public Dropdown myDropdown;
+    public List<PlayerController> players;
+
+    // Use this for initialization
+    void Start () {
+        myDropdown.onValueChanged.AddListener(delegate {
+            myDropdownValueChangedHandler(myDropdown);
+        });
+    }
+    void Destroy()
+    {
+        myDropdown.onValueChanged.RemoveAllListeners();
+    }
+    public void myDropdownValueChangedHandler(Dropdown target)
+    {
+       if (target.value == 0)
+        {
+            foreach (var item in players)
+            {
+                item.aiType = PlayerController.AIType.V2;
+            }
+        }
+        if (target.value == 1)
+        {
+            foreach (var item in players)
+            {
+                item.aiType = PlayerController.AIType.V1;
+            }
+        }
+        if (target.value == 2)
+        {
+            foreach (var item in players)
+            {
+                item.aiType = PlayerController.AIType.V0;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
